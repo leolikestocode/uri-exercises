b58b522b8580794b99f5ff42cf37998b2e284029
leolikestocode <leonardofpereira25@gmail.com>
2021-04-30 22:03:44 -0300

    1546 solved

---
+++ ad-hoc/1546.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+for i in range(n):
+    m = int(input())
+
+    for j in range(m):
+        l = int(input())
+
+        if l == 1:
+            print("Rolien")
+        if l == 2:
+            print("Naej")
+        if l == 3:
+            print("Elehcim")
+        if l == 4:
+            print("Odranoel")
