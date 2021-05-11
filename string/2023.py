67155772a8d7284379316c1a1495fa80bcc62406
leolikestocode <leonardofpereira25@gmail.com>
2021-05-11 15:52:23 -0300

    2023 solved

---
+++ string/2023.py
@@ -0,0 +1,10 @@
+kid = []
+
+while True:
+    try:
+        kid.append(input())
+
+    except EOFError:
+        kid = sorted(kid, key=lambda s: s.lower())
+        print(kid[len(kid)-1])
+        break
